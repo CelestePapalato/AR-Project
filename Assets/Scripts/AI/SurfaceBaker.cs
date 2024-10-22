719d17facf4915d1ce01925d47ce0b257f72daf8
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-10-22 16:50:40 -0300

    Creado SurfaceBaker. Modificado prefab de ARPlane para que tengan el componente Nav Mesh Surface.

---
+++ Assets/Scripts/AI/SurfaceBaker.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Linq;
+using Unity.AI.Navigation;
+
+public class SurfaceBaker : MonoBehaviour
+{
+    public ARPlaneManager planeManager;
+
+    private void OnEnable()
+    {
+        planeManager.planesChanged += UpdatePlaneList;
+    }
+
+    private void OnDisable()
+    {
+        planeManager.planesChanged -= UpdatePlaneList;
+    }
+
+    Dictionary<ARPlane, NavMeshSurface> surfaces = new Dictionary<ARPlane, NavMeshSurface>();
+
+    ARPlane[] toUpdate;
+
+    void UpdatePlaneList(ARPlanesChangedEventArgs args)
+    {
+        List<ARPlane> planesToBake = new List<ARPlane>();
+        if (args.removed.Count > 0)
+        {
+            foreach(ARPlane plane in args.removed)
+            {
+                surfaces.Remove(plane);
+            }
+        }
+        if (args.updated.Count > 0)
+        {
+            planesToBake.AddRange(args.updated);
+        }
+
+        planesToBake.AddRange(args.added);
+
+        foreach(ARPlane plane in args.added)
+        {
+            surfaces[plane] = plane.GetComponent<NavMeshSurface>();
+        }
+
+        toUpdate = planesToBake.ToArray();
+    }
+
+    [ContextMenu("Bake")]
+    void BakeSurfaces()
+    {
+        if(toUpdate.Length > 0)
+        {
+            foreach (ARPlane plane in toUpdate)
+            {
+                surfaces[plane].BuildNavMesh();
+            }
+        }
+    }
+}
